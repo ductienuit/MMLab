70c099dbebbaf365999e9ee191da1c3b11e02a76
Duc Tien <ductien.uit@gmail.com>
2018-08-23 16:33:51 +0700

    create modal faceResult

--- app/src/main/java/vn/edu/uit/mmlab/modal/FaceResult.java
+++ app/src/main/java/vn/edu/uit/mmlab/modal/FaceResult.java
@@ -1,4 +1,87 @@
 package vn.edu.uit.mmlab.modal;
 
+import java.util.ArrayList;
+
 public class FaceResult {
+    public ArrayList<FaceInfor> getResult() {
+        return result;
+    }
+
+    public void setResult(ArrayList<FaceInfor> result) {
+        this.result = result;
+    }
+
+    public FaceResult(ArrayList<FaceInfor> result) {
+        this.result = result;
+    }
+
+    ArrayList<FaceInfor> result;
+    public class FaceInfor{
+        String name;
+
+        public FaceInfor(String name, String h, String w, String y, String x, String probs) {
+            this.name = name;
+            this.h = h;
+            this.w = w;
+            this.y = y;
+            this.x = x;
+            this.probs = probs;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getH() {
+            return h;
+        }
+
+        public void setH(String h) {
+            this.h = h;
+        }
+
+        public String getW() {
+            return w;
+        }
+
+        public void setW(String w) {
+            this.w = w;
+        }
+
+        public String getY() {
+            return y;
+        }
+
+        public void setY(String y) {
+            this.y = y;
+        }
+
+        public String getX() {
+            return x;
+        }
+
+        public void setX(String x) {
+            this.x = x;
+        }
+
+        public String getProbs() {
+            return probs;
+        }
+
+        public void setProbs(String probs) {
+            this.probs = probs;
+        }
+
+        String h;
+        String w;
+        String y;
+        String x;
+        String probs;
+    }
 }
+
+
---
+++ app/src/main/java/vn/edu/uit/mmlab/network/RetrofitClientInstance.java
@@ -0,0 +1,4 @@
+package vn.edu.uit.mmlab.network;
+
+public class RetrofitClientInstance {
+}
